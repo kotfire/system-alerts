1f60f0bee215a46e56de959227d2d23e62286a1c
Gustavo Real <KotDesigns@gmail.com>
2015-04-17 12:27:22 +0200

    allow to set up datetime in add alert command

--- src/Kotfire/SystemAlerts/Commands/AddAlertCommand.php
+++ src/Kotfire/SystemAlerts/Commands/AddAlertCommand.php
@@ -6,6 +6,7 @@ use Kotfire\SystemAlerts\SystemAlert as SystemAlert;
 use Kotfire\SystemAlerts\Alert as Alert;
 use Illuminate\Console\Command;
 use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
 
 class AddAlertCommand extends Command
 {
@@ -22,7 +23,7 @@ class AddAlertCommand extends Command
 
     public function fire()
     {
-        $this->systemAlert->addAlert($this->argument('message'), Alert::INFO_TYPE);
+        $this->systemAlert->addAlert($this->argument('message'), Alert::INFO_TYPE, $this->option('datetime'));
     }
 
     /**
@@ -34,6 +35,18 @@ class AddAlertCommand extends Command
     {
         return [
             ['message', InputArgument::REQUIRED, 'Alert message']
+        ];
+    }
+
+    /**
+     * Get the console command options.
+     *
+     * @return array
+     */
+    protected function getOptions()
+    {
+        return [
+            ['datetime', 'd', InputOption::VALUE_OPTIONAL, 'Alert datetime'],
         ];
     }
 }
